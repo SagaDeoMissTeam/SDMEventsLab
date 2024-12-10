4ef5dd1e9d9aa32d6d99f45ff758b862d84afcff
DESKTOP-23JNNV9\Wobblecraft <artur.engibaryan2003@gmail.com>
2024-12-10 15:16:48 +0200

    tese2

--- src/main/java/net/sixik/sdmeventslab/events/EventManager.java
+++ src/main/java/net/sixik/sdmeventslab/events/EventManager.java
@@ -2,17 +2,23 @@ package net.sixik.sdmeventslab.events;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.sixik.sdmeventslab.events.conditions.EventCondition;
 import net.sixik.sdmeventslab.register.EventsRegisters;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class EventManager {
     public static final EventManager INSTANCE = new EventManager();
     public List<EventBase> mayStarted = new ArrayList<>();
+    public HashMap<UUID,List<ResourceLocation>> activeOnPlayer = new HashMap<>();
 
     public void checkProperties(MinecraftServer server){
 
+        mayStarted.clear();
         for (EventBase e : EventsRegisters.getEvents().values()) {
 
             int checkPoint = 0;
@@ -33,8 +39,43 @@ public class EventManager {
 
             if (checkPoint == e.properties.whiteList.size()) {
                 mayStarted.add(e);
-                System.out.println("Work");
             };
+
+            if (e.eventSide == EventBase.EventSide.LOCAL){
+                for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+                    boolean isBreak = false;
+                    for (EventCondition c : e.conditions2) {
+
+                        if(!c.canExecuteLocal(player)) {
+                            isBreak = true;
+                            break;
+                        };
+
+                    }
+                    if(!isBreak && mayStarted.contains(e)) {
+                        e.onEventStart(server);
+                        if(!activeOnPlayer.containsKey(player.getGameProfile().getId()))
+                            activeOnPlayer.put(player.getGameProfile().getId(),new ArrayList<>());
+                        activeOnPlayer.get(player.getGameProfile().getId()).add(e.eventID);
+                    }
+
+                }
+            }else{
+                    boolean isBreak = false;
+                    for (EventCondition c : e.conditions2) {
+                       if(!c.canExecuteGlobal(server)) {
+                           isBreak = true;
+                           break;
+                       };
+
+                    }
+                    if(!isBreak && mayStarted.contains(e)) e.onEventStart(server);
+                    for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+                        if(!activeOnPlayer.containsKey(player.getGameProfile().getId()))
+                            activeOnPlayer.put(player.getGameProfile().getId(),new ArrayList<>());
+                        activeOnPlayer.get(player.getGameProfile().getId()).add(e.eventID);
+                    }
+            }
 
         }
     };
